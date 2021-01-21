d8c01d1d3637a4873f0a76028b3539b102066e60
prasath015 <prasath017@gmail.com>
2021-01-21 11:11:05 +0530

    commit

--- farmersystem/src/components/CreateSupplierComponent.jsx
+++ farmersystem/src/components/CreateSupplierComponent.jsx
@@ -19,11 +19,6 @@ const passwordRegex = RegExp(
 const formValid = ({ formErrors, ...rest }) => {
   let valid = true;
 
-  // validate form errors being empty
-  Object.values(formErrors).forEach(val => {
-    val.length > 0 && (valid = false);
-  });
-
   // validate the form was filled out
   Object.values(rest).forEach(val => {
     val === null && (valid = false);
